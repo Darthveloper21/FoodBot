b87c58719826a3720d34e69632ea45d6bf79d5fc
Darthveloper21 <hienvungoc01@gmail.com>
2021-06-12 16:50:45 +0700

    Finish preprocessing for diner values

--- database/database.py
+++ database/database.py
@@ -11,15 +11,14 @@ mydb = mysql.connector.connect(
     database="FindFood"
 )
 
-cursor = mydb.cursor()
+cursor = mydb.cursor(buffered=True)
 
 path_bun = "../save/link_store_bún_"
 path_com = "../save/link_store_cơm_"
 path_pho = "../save/link_store_phở_"
 
 
-def add_diner(name, address, city, district, price_min, price_max, website,
-              price_point, quality_point, destination_point, service_point, space_point):
+def add_diner(name, address, city, district, price_min, price_max, website, review_point):
     """
     Add diner information, id auto_increment
     :param name: str diner name
@@ -29,24 +28,23 @@ def add_diner(name, address, city, district, price_min, price_max, website,
     :param price_min: float minimum price range
     :param price_max: float maximum price range
     :param website: str foody site
-    :param price_point: float %.2f price review
-    :param quality_point: float %.2f food quality review
-    :param destination_point: float %.2f destination review
-    :param service_point: float %.2f service review
-    :param space_point: float %.2f layout review
+    :param review_point: list of review point for each aspects in format %.2f
     :return: None
     """
-    pass
+    global mydb
+    global cursor
+    values = [name, address, city, district, price_min, price_max, website] + prep_review(review_point)
+    print(values)
 
 
 def add_menu(name, price, diner_id, details=None):
     """
-
+    add dished to menu table
     :param name: str dish name
     :param price: float dish price
-    :param diner_id:
-    :param details:
-    :return:
+    :param diner_id: int specify which diner
+    :param details: dish description
+    :return: None
     """
     pass
 
@@ -55,7 +53,17 @@ def get_diner_id():
     """
     :return: the next diner id
     """
-    pass
+    global mydb
+    global cursor
+
+    sqlLine = 'SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES ' \
+              'WHERE TABLE_SCHEMA = \'FindFood\' AND TABLE_NAME = \'diners\';'
+
+    cursor.execute(sqlLine)
+    rt = cursor.next()[0]
+    if rt is None:
+        return 1
+    return rt
 
 
 def prep_time(timetable: list, diner_id):
@@ -77,8 +85,20 @@ def prep_review(review_point: list):
     :param review_point: list of points of each aspects
     :return: list of review_point in order: Chất lượng, Giá cả, Phục vụ, Vị trí, Không gian
     """
+    reviews = [None] * 5
+
     for x in review_point:
-        print(x)
+        if list(x.keys())[0] == 'Chất lượng':
+            reviews[0] = x['Chất lượng']
+        if list(x.keys())[0] == 'Giá cả':
+            reviews[1] = x['Giá cả']
+        if list(x.keys())[0] == 'Phục vụ':
+            reviews[2] = x['Phục vụ']
+        if list(x.keys())[0] == 'Vị trí':
+            reviews[3] = x['Vị trí']
+        if list(x.keys())[0] == 'Không gian':
+            reviews[4] = x['Không gian']
+    return reviews
 
 
 def load_in_range(food_path, left, right):
@@ -96,15 +116,16 @@ def load_in_range(food_path, left, right):
             file = open(path_tmp + '/' + f, 'r', encoding='utf8')
             data = json.load(file)
             try:
-                prep_time(data['Time'])
-                # add_diner(data['name'],
-                #           data['address'],
-                #           data['city'],
-                #           data['district'],
-                #           data['priceMin'],
-                #           data['priceMax'],
-                #           data['review_point'])
-                prep_review(data['review_point'])
+                diner_id = get_diner_id()
+                add_diner(data['name'],
+                          data['address'],
+                          data['city'],
+                          data['district'],
+                          data['priceMin'],
+                          data['priceMax'],
+                          data['website'],
+                          data['review_point'])
+                prep_time(data['Time'], diner_id)
             except IndexError:
                 print('no menu')
             file.close()
