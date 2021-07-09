48d851a43847ed157061259058fa115fb44d895f
Darthveloper21 <hienvungoc01@gmail.com>
2021-07-09 11:52:49 +0700

    fixing mysql search vietnamese text

--- database/diner.sql
+++ database/diner.sql
@@ -8,13 +8,13 @@ DROP TABLE IF EXISTS diners;
 
 CREATE TABLE diners (
 	id INT(20) NOT NULL AUTO_INCREMENT,
-	name TEXT NOT NULL,
-	address TEXT NOT NULL,
-	city TEXT NOT NULL,
-	district TEXT,
+	name TEXT  NOT NULL COLLATE 'utf16_vietnamese_ci',
+	address TEXT NOT NULL COLLATE 'utf16_vietnamese_ci',
+	city TEXT NOT NULL COLLATE 'utf16_vietnamese_ci',
+	district TEXT COLLATE 'utf16_vietnamese_ci',
 	pricemin DECIMAL(12, 2) NOT NULL,
 	pricemax DECIMAL(12, 2) NOT NULL,
-	website TEXT,
+	website TEXT COLLATE 'utf16_vietnamese_ci',
 	qualityPoint FLOAT(5, 2),
 	pricePoint FLOAT(5, 2),
 	servicePoint FLOAT(5, 2),
@@ -25,9 +25,9 @@ CREATE TABLE diners (
 
 CREATE TABLE menu (
 	foodID INT(20) NOT NULL AUTO_INCREMENT,
-	name TEXT NOT NULL,
+	name TEXT NOT NULL COLLATE 'utf16_vietnamese_ci',
 	price VARCHAR(20) NOT NULL,
-	details TEXT,
+	details TEXT COLLATE 'utf16_vietnamese_ci',
 	dinerID INT(20) NOT NULL,
 	PRIMARY KEY (foodID),
 	CONSTRAINT myfood FOREIGN KEY (dinerID) REFERENCES diners(id) ON UPDATE CASCADE
