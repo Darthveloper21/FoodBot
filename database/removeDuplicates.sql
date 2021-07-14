db7c00ff96d13431420ed1056eea1d2d0bd15d55
datDat <32026170+cuocchie@users.noreply.github.com>
2021-07-14 17:20:37 +0700

    Update removeDuplicates.sql
    Add all attributes in comparison.

--- database/removeDuplicates.sql
+++ database/removeDuplicates.sql
@@ -6,31 +6,44 @@ use findfood;
 -- SELECT id from diners table which is duplicated,
 -- Uncomment block below to examine duplicated records.
 --
--- SELECT id FROM (SELECT id, name, ROW_NUMBER()   
--- OVER (PARTITION BY name ORDER BY name) AS row_num   
--- FROM diners) AS temp_table WHERE row_num>1;  
+--  SELECT id FROM (SELECT id, ROW_NUMBER()   
+--        OVER (PARTITION BY name, address, city, district, pricemin, pricemax, website, qualityPoint, pricePoint, servicePoint, destinationPoint, spacePoint
+-- 			ORDER BY name) AS row_num   
+--     FROM diners) AS temp_table WHERE row_num > 1;
 -- 
 
 -- DELETE duplicated in menu
 DELETE FROM menu 
-WHERE dinerID IN(  
-    SELECT id FROM (SELECT id, ROW_NUMBER()   
-       OVER (PARTITION BY name ORDER BY name) AS row_num   
-    FROM diners) AS temp_table WHERE row_num > 1);
+WHERE dinerID IN (  
+    SELECT id FROM (
+		SELECT id, ROW_NUMBER()   
+		OVER (
+			PARTITION BY name, address, city, district, pricemin, pricemax, website, qualityPoint, pricePoint, servicePoint, destinationPoint, spacePoint
+			ORDER BY name) AS row_num   
+		FROM diners) AS temp_table 
+	WHERE row_num > 1);
 --
 
 -- DELETE duplicated in timetable
 DELETE FROM timetable 
-WHERE dinerID IN(  
-    SELECT id FROM (SELECT id, ROW_NUMBER()   
-       OVER (PARTITION BY name ORDER BY name) AS row_num   
-    FROM diners) AS temp_table WHERE row_num>1);
+WHERE dinerID IN (  
+     SELECT id FROM (
+		SELECT id, ROW_NUMBER()   
+		OVER (
+			PARTITION BY name, address, city, district, pricemin, pricemax, website, qualityPoint, pricePoint, servicePoint, destinationPoint, spacePoint
+			ORDER BY name) AS row_num   
+		FROM diners) AS temp_table 
+	WHERE row_num > 1);
 --
 
 -- DELETE duplicated in diners
 DELETE FROM diners 
-WHERE id IN(  
-    SELECT id FROM (SELECT id, ROW_NUMBER()   
-       OVER (PARTITION BY name ORDER BY name) AS row_num   
-    FROM diners) AS temp_table WHERE row_num>1);
+WHERE id IN (  
+	SELECT id FROM (
+		SELECT id, ROW_NUMBER()   
+		OVER (
+			PARTITION BY name, address, city, district, pricemin, pricemax, website, qualityPoint, pricePoint, servicePoint, destinationPoint, spacePoint
+			ORDER BY name) AS row_num   
+		FROM diners) AS temp_table 
+	WHERE row_num > 1);
 --
